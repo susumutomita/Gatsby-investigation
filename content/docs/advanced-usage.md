167991be7a0e905a328b6ea5eb9327fb782c50d1
susumu tomita <oyster880@gmail.com>
2024-10-30 09:14:03 +0900

    add pagelinks

---
+++ content/docs/advanced-usage.md
@@ -0,0 +1,9 @@
+---
+title: "Advanced Usage"
+date: "2024-02-20"
+slug: "/docs/advanced-usage"
+---
+
+# Advanced Usage
+
+This guide covers advanced topics... 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,44 @@
 import React from 'react'
+import { graphql, Link } from 'gatsby'
 import Layout from '../components/layout'
 
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
+  const posts = data.allMarkdownRemark.nodes
+
   return (
     <Layout>
       <h1>Welcome to Markdown Docs</h1>
-      <p>This is the home page of your documentation site.</p>
+      <div>
+        {posts.map(post => (
+          <article key={post.id}>
+            <h2>
+              <Link to={post.frontmatter.slug || `/post/${post.id}`}>
+                {post.frontmatter.title}
+              </Link>
+            </h2>
+            <small>{post.frontmatter.date}</small>
+            <p>{post.excerpt}</p>
+          </article>
+        ))}
+      </div>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { frontmatter: { date: DESC } }) {
+      nodes {
+        id
+        excerpt
+        frontmatter {
+          title
+          date(formatString: "MMMM DD, YYYY")
+          slug
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
