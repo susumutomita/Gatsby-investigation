3ac6f1b21d30856971c394fe212384cdad280387
susumu tomita <oyster880@gmail.com>
2024-11-10 06:49:54 +0900

    fix build

---
+++ src/src/components/ProtectedContent.js
@@ -0,0 +1,13 @@
+// src/components/ProtectedContent.js
+import React from 'react';
+
+const ProtectedContent = () => {
+  return (
+    <div>
+      <h1>Protected Content</h1>
+      <p>This content is only visible to authenticated users.</p>
+    </div>
+  );
+};
+
+export default ProtectedContent;
